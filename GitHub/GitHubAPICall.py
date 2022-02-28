52203ca1f4645e04da9b7c51a2a7d28b28edc109
KoalaUniverse <koalauniverse@gmail.com>
2022-02-28 14:36:58 +0100

    Updated the program to use a supplied token instead of reading one from a file
    This should make testing easier for now.

--- GitHub/GitHubAPICall.py
+++ GitHub/GitHubAPICall.py
@@ -8,18 +8,18 @@ class GitHubAPICall:
         self.base_url_users = "https://api.github.com/users/"
 
     # Get the information of the owner, of the repository, and of the specific version.
-    def get_all_data(self, owner, repo, version):
-        repository_data = self.GetData(self.base_url_repos + owner + '/' + repo)
-        version_data = self.GetData(self.base_url_repos + owner + '/' + repo + '/releases/tags/' + version)
-        user_data = self.GetData(self.base_url_users + owner)
+    def get_all_data(self, owner, repo, version, user_token):
+        repository_data = self.GetData(self.base_url_repos + owner + '/' + repo, user_token)
+        version_data = self.GetData(self.base_url_repos + owner + '/' + repo + '/releases/tags/' + version, user_token)
+        user_data = self.GetData(self.base_url_users + owner, user_token)
         
         return (repository_data, version_data, user_data)
     
     # Perform a simple GET request, based off the given URL
-    def get_data(self, api_url):
+    def get_data(self, api_url, user_token):
         # Basic request to get the information.
         data_response = requests.get(api_url,
-                                headers={'Authorization': 'token ' + json.load(open('secret.json'))['user_token'], # TEMPORARY USER TOKEN FOR TESTING
+                                headers={'Authorization': 'token ' + user_token, # TEMPORARY USER TOKEN FOR TESTING
                                 'accept': 'application/vnd.github.v3+json'})
 
         # See if we got a valid response
