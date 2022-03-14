f059b4a9b2a9c6df5e23ad629073b5b355a47013
mastercoder10 <58420432+mastercoder10@users.noreply.github.com>
2022-03-14 20:58:08 +0100

    Update LibrariesioAPI.py

--- Libraries.io/LibrariesioAPI.py
+++ Libraries.io/LibrariesioAPI.py
@@ -18,6 +18,7 @@ class LibrariesioAPICall:
         print(data_response.json())
     
     # Get breakdown of SourceRank score for a given project
+    # For example, platform: NPM, name: base62, api_key: API key of the user
     def get_project_sourcerank(self, platform, name, api_key):
         data_response = requests.get('https://libraries.io/api/' + str(platform) + '/' + str(name) + '/sourcerank?api_key=' + str(api_key))
         print(data_response.json())
