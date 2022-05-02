05ac2248bb22f74cf05b232e6213aff0b89e352c
VaHoffmann <46539474+VaHoffmann@users.noreply.github.com>
2022-05-02 16:32:37 +0200

    Additional Comments

--- Stack Overflow/Stack_Overflow_calls.py
+++ Stack Overflow/Stack_Overflow_calls.py
@@ -14,11 +14,14 @@ class StackOverflowCall:
 
         # Take the url for Stack Overflow trends
         url = "https://insights.stackoverflow.com/trends/get-data"
+
         # Extract the data as json
         response = requests.get(url).json()
-        #
+
+        # Devide the json into data and percentage data
         years = response.get("Year")
         months = response.get("Month")
         popularity = response.get("TagPercents").get(language)
 
+        # return list of date and percentage tuples.
         return list(zip(months, years, popularity, strict=True))
